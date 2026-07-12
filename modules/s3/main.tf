0ccd22bc338b1479cee2046bd236d5223e2f9163
Matishac14 <matias.fernandez.zamorano@gmail.com>
2026-07-12 15:57:11 -0400

    FIX: corregir sintaxis de supresiones checkov y agregar ciclo de vida s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,9 +1,11 @@
-#Creación del bucket S3 con nombre único global.
-# checkov:skip=CKV_AWS_18: Se omite Access Logging para evitar costos de almacenamiento doble en el laboratorio.
-# checkov:skip=CKV_AWS_144: Se omite Cross-Region Replication para evitar costos de transferencia de datos.
-#checkov:skip=CKV2_AWS_62: No se requiere enrutamiento de notificaciones (EventBridge/SNS) en esta arquitectura.
+# Creación del bucket S3 con nombre único global.
 resource "aws_s3_bucket" "this" {
   bucket = "${var.project_name}-${var.environment}-${var.bucket_name}"
+
+  # checkov:skip=CKV_AWS_18: Se omite Access Logging para evitar costos de almacenamiento doble.
+  # checkov:skip=CKV_AWS_144: Se omite Cross-Region Replication para evitar costos de transferencia de datos.
+  # checkov:skip=CKV2_AWS_62: No se requiere enrutamiento de notificaciones (EventBridge/SNS) en esta arquitectura.
+  # checkov:skip=CKV_AWS_145: Se utiliza cifrado nativo SSE-S3 (AES256) en lugar de KMS gestionado para mitigar costos.
 
   tags = merge(var.tags, {
     Name        = "${var.project_name}-${var.environment}-${var.bucket_name}-bucket"
@@ -11,17 +13,16 @@ resource "aws_s3_bucket" "this" {
   })
 }
 
-#Bloqueo de acceso público al bucket S3.
+# Bloqueo de acceso público al bucket S3.
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
-
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-#Control de versiones.
+# Control de versiones.
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
@@ -29,14 +30,26 @@ resource "aws_s3_bucket_versioning" "this" {
   }
 }
 
-#Cifrado en reposo por default.
-# checkov:skip=CKV_AWS_61:Se omite el uso de KMS para cifrado en reposo por simplicidad.
+# Cifrado en reposo por default.
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Política de Ciclo de Vida (FinOps - Checkov CKV2_AWS_61)
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    id     = "auto-delete-old-versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 7
     }
   }
 }
